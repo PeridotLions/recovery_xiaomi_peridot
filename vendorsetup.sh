d6ec548706bfbaf41e5ffc93ca76e03dcdabc58a
PugzAreCute <me@pugzarecute.com>
2024-12-21 16:40:46 +0530

    peridot: fixup vendorsetup

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo fox_peridot-user
-add_lunch_combo fox_peridot-userdebug
-add_lunch_combo fox_peridot-eng
