18872dc28a0a563616d8533d6378a64d8ae332b6
PugzAreCute <me@pugzarecute.com>
2024-12-21 15:36:12 +0530

    peridot: initial recovery tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/fox_peridot.mk
+
+COMMON_LUNCH_CHOICES := \
+    fox_peridot-user \
+    fox_peridot-userdebug \
+    fox_peridot-eng
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# recovery_xiaomi_peridot
+# Android device tree for POCO 24069PC21G (peridot)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
