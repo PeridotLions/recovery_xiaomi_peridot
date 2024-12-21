43cf399a3b486de4dc115f01fcfcf693e32b7acc
PugzAreCute <me@pugzarecute.com>
2024-12-21 16:37:09 +0530

    peridot: set shipping api level

--- device.mk
+++ device.mk
@@ -12,6 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 LOCAL_PATH := device/xiaomi/peridot
+
+#API
+PRODUCT_SHIPPING_API_LEVEL  := 34
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
