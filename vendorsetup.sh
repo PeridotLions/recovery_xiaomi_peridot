fce42be207f5c14678d383b730b2565bc5752ed1
PugzAreCute <me@pugzarecute.com>
2024-12-21 15:36:12 +0530

    peridot: initial recovery tree

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo fox_peridot-user
+add_lunch_combo fox_peridot-userdebug
+add_lunch_combo fox_peridot-eng
